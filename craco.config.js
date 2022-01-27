4964296588dab431f4a98bcab95930ab5aedc760
Damian Sznajder <dsznajder@users.noreply.github.com>
2022-01-27 23:10:57 +0100

    fix: aliases for prod build (#47)

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,21 @@
 const { whenProd } = require('@craco/craco')
 const cssnano = require('cssnano')
+const CracoAlias = require('craco-alias')
 
 module.exports = {
   eslint: {
     mode: 'file',
   },
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: './src',
+        tsConfigPath: './tsconfig.json',
+      }
+    }
+  ],
   style: {
     postcss: {
       plugins: (plugins) => whenProd(() => [...plugins, cssnano], []),
