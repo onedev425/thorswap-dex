66dfb2e38a0d65830d61df936c3484941db82d97
0x Mars <83092433+0xMars4204@users.noreply.github.com>
2022-02-16 06:55:22 +0000

    feat: add custom scrollbar (#120)

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,7 @@
 import { ReactNode, useState } from 'react'
 
+import { Scrollbars } from 'react-custom-scrollbars'
+
 import classNames from 'classnames'
 
 import { Header } from 'components/Header'
@@ -28,28 +30,39 @@ export const Layout = ({ children }: LayoutProp) => {
   }
 
   return (
-    <div className="relative flex flex-col w-full min-h-screen mx-auto my-0 max-w-8xl">
-      <aside className="fixed hidden bg-fuchsia-100 md:block">
-        <Sidebar />
-      </aside>
-      <main
-        className={classNames(
-          'flex flex-col ml-0 md:ml-[92px] md:max-w-[calc(100%-148px)] px-10 py-5 dark:bg-elliptical',
-          { 'blur-md pointer-events-none': onOff },
-        )}
-      >
-        <Header
-          currencyOptions={menuItems}
-          currency={currency}
-          selectCurrency={setCurrency}
-          priceLabel="1ᚱ = $ 10.04"
-          gweiLabel="156 GWEI"
-          connectWallet={() => {}}
-          openDrawer={connectWallet}
-          refresh={() => {}}
+    <Scrollbars
+      autoHide
+      renderThumbVertical={({ style, ...scrollProps }) => (
+        <div
+          style={{ ...style, backgroundColor: '#00d2ff', width: '4px' }}
+          {...scrollProps}
         />
-        {children}
-      </main>
-    </div>
+      )}
+      style={{ height: '100vh' }}
+    >
+      <div className="relative flex flex-col w-full min-h-screen mx-auto my-0 max-w-8xl">
+        <aside className="fixed hidden bg-fuchsia-100 md:block">
+          <Sidebar />
+        </aside>
+        <main
+          className={classNames(
+            'flex flex-col ml-0 md:ml-[92px] md:max-w-[calc(100%-148px)] px-10 py-5 dark:bg-elliptical',
+            { 'blur-md pointer-events-none': onOff },
+          )}
+        >
+          <Header
+            currencyOptions={menuItems}
+            currency={currency}
+            selectCurrency={setCurrency}
+            priceLabel="1ᚱ = $ 10.04"
+            gweiLabel="156 GWEI"
+            connectWallet={() => {}}
+            openDrawer={connectWallet}
+            refresh={() => {}}
+          />
+          {children}
+        </main>
+      </div>
+    </Scrollbars>
   )
 }
