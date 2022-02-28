020dda44b9fa0ec54e39aa0b067d7105dfbf9a35
Marek <m.sadura@outlook.com>
2022-02-28 21:11:20 +0100

    feat: use native select on small screens (#156)

--- src/components/Atomic/Select/Select.tsx
+++ src/components/Atomic/Select/Select.tsx
@@ -1,6 +1,9 @@
 import { useCallback } from 'react'
 
-import { Button, Row } from 'components/Atomic'
+import classNames from 'classnames'
+
+import { Box, Button, Typography } from 'components/Atomic'
+import { genericBgClasses } from 'components/constants'
 
 export type Props = {
   options: string[]
@@ -19,17 +22,41 @@ export const Select = (props: Props) => {
   )
 
   return (
-    <Row className="gap-2">
-      {options.map((option, index) => (
-        <Button
-          key={option}
-          variant="tint"
-          type={activeIndex !== index ? 'outline' : 'default'}
-          onClick={() => onHandleChange(index)}
+    <>
+      <Box className="hidden md:flex gap-2">
+        {options.map((option, index) => (
+          <Button
+            key={option}
+            variant="tint"
+            type={activeIndex !== index ? 'outline' : 'default'}
+            onClick={() => onHandleChange(index)}
+          >
+            <Typography variant="caption-xs">{option}</Typography>
+          </Button>
+        ))}
+      </Box>
+
+      <Box className="md:hidden">
+        <select
+          className={classNames(
+            'block w-full pl-3 pr-5 py-2 text-base border focus:outline-none focus:ring-none focus:border-transparent sm:text-sm rounded-2xl text-light-typo-primary dark:text-dark-typo-primary',
+            'border-light-border-primary dark:border-dark-border-primary focus:border-light-border-primary dark:focus:border-dark-border-primary',
+            genericBgClasses.secondary,
+          )}
+          value={activeIndex}
+          onChange={(e) => onHandleChange(Number(e.target.value))}
         >
-          {option}
-        </Button>
-      ))}
-    </Row>
+          {options.map((option, index) => (
+            <option
+              className="text-dark-typo-primary"
+              value={index}
+              key={option}
+            >
+              {option}
+            </option>
+          ))}
+        </select>
+      </Box>
+    </>
   )
 }
