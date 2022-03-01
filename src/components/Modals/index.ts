66df4511606e500b188f962103f0a5673f63c3bd
0x Mars <marsoon4204@gmail.com>
2022-03-01 15:26:38 +0000

    chore: remove unnecessary modal

--- src/components/Modals/index.ts
+++ src/components/Modals/index.ts
@@ -1,4 +1,3 @@
 export { ConnectKeystore } from './ConnectKeystore/ConnectKeystore'
-export { ConnectWallet } from './ConnectWallet/ConnectWallet'
 export { SelectChain } from './SelectChain/SelectChain'
 export * from './types'
