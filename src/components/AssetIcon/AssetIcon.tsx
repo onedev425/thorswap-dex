60a38ee309746a225e6030a806642a3ec05db641
0x Mars <83092433+0xMars4204@users.noreply.github.com>
2022-03-08 13:32:34 +0000

    feat: add fallback icon for AssetIcon (#206)

--- src/components/AssetIcon/AssetIcon.tsx
+++ src/components/AssetIcon/AssetIcon.tsx
@@ -1,12 +1,17 @@
 import { SupportedChain } from '@thorswap-lib/multichain-sdk'
 import classNames from 'classnames'
 
-import { Typography } from 'components/Atomic'
+import { Box, Typography } from 'components/Atomic'
 import { ChainIcon } from 'components/ChainIcon'
 
 import { genericBgClasses } from '../constants'
 import { iconSizes, AssetIconProps } from './types'
-import { getAssetIconUrl, getSecondaryIconPlacementStyle } from './utils'
+import {
+  getAssetIconUrl,
+  getIntFromName,
+  getSecondaryIconPlacementStyle,
+  rainbowStop,
+} from './utils'
 
 export const AssetIcon = ({
   className,
@@ -20,15 +25,23 @@ export const AssetIcon = ({
   const iconUrl = getAssetIconUrl(asset)
   const secondaryIconSize = hasChainIcon ? iconSize * 0.4 : 0
 
+  // fallback icon
+  const tickerNums = getIntFromName(asset.ticker)
+  const fallbackBgImg = `linear-gradient(45deg, ${rainbowStop(
+    tickerNums[0],
+  )}, ${rainbowStop(tickerNums[1])})`
+
   return (
     <div className="relative flex">
-      <div
-        style={{ width: iconSize, height: iconSize }}
+      <Box
         className={classNames(
           className,
-          'flex rounded-full items-center justify-center p-2 box-border overflow-hidden relative',
+          'rounded-full box-border overflow-hidden relative',
           { [genericBgClasses[bgColor as 'blue']]: bgColor },
         )}
+        center
+        width={iconSize}
+        height={iconSize}
       >
         {iconUrl ? (
           <img
@@ -38,9 +51,18 @@ export const AssetIcon = ({
             alt={asset.symbol}
           />
         ) : (
-          <Typography variant="caption">{asset.symbol}</Typography>
+          <Box
+            center
+            width={iconSize}
+            height={iconSize}
+            style={{ backgroundImage: fallbackBgImg }}
+          >
+            <Typography variant="caption">
+              {asset.ticker.slice(0, 3)}
+            </Typography>
+          </Box>
         )}
-      </div>
+      </Box>
 
       {hasChainIcon && asset.type !== 'Native' && (
         <div
--- src/components/AssetIcon/utils.ts
+++ src/components/AssetIcon/utils.ts
@@ -13,6 +13,26 @@ import { SecondaryIconPlacement } from './types'
 const twBaseUri =
   'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains'
 
+export const checkIfImageExists = (url: string): boolean => {
+  const img = new Image()
+
+  img.src = url
+
+  if (img.complete) {
+    return true
+  } else {
+    img.onload = () => {
+      return true
+    }
+
+    img.onerror = () => {
+      return false
+    }
+  }
+
+  return false
+}
+
 export const getAssetIconUrl = (asset: Asset) => {
   if (Object.keys(customIconMap).includes(asset.ticker)) {
     return customIconMap[asset.ticker as CustomIconType]
@@ -28,8 +48,9 @@ export const getAssetIconUrl = (asset: Asset) => {
     }
 
     const contract = multichain.eth.getCheckSumAddress(asset)
+    const logoUrl = `${twBaseUri}/ethereum/assets/${contract}/logo.png`
 
-    return `${twBaseUri}/ethereum/assets/${contract}/logo.png`
+    return checkIfImageExists(logoUrl) ? logoUrl : ''
   }
 
   const logoSymbol = assetIconMap[asset.ticker as BepIconType]
@@ -37,7 +58,8 @@ export const getAssetIconUrl = (asset: Asset) => {
     return `${twBaseUri}/binance/assets/${logoSymbol}/logo.png`
   }
 
-  return `${twBaseUri}/binance/assets/${asset.symbol}/logo.png`
+  const bep2LogoUrl = `${twBaseUri}/binance/assets/${asset.symbol}/logo.png`
+  return checkIfImageExists(bep2LogoUrl) ? bep2LogoUrl : ''
 }
 
 export const getSecondaryIconPlacementStyle = (
@@ -60,4 +82,29 @@ export const getSecondaryIconPlacementStyle = (
       return { bottom: 0, right: -offset }
     }
   }
+}
+
+export const rgb2hex = (r: number, g: number, b: number) =>
+  `#${((r << 16) + (g << 8) + b).toString(16).padStart(6, '0')}`
+
+export const rainbowStop = (h: number) => {
+  const f = (n: number, k = (n + h * 12) % 12) =>
+    0.5 - 0.5 * Math.max(Math.min(k - 3, 9 - k, 1), -1)
+  return rgb2hex(
+    Math.floor(f(0) * 255),
+    Math.floor(f(8) * 255),
+    Math.floor(f(4) * 255),
+  )
+}
+
+export const getIntFromName = (str: string): number[] => {
+  const inputStr = String(str).toUpperCase()
+
+  const div = 22
+
+  const firstInt = (inputStr.charCodeAt(0) - 'A'.charCodeAt(0)) / div
+  const secondInt =
+    inputStr.length > 1 ? (inputStr.charCodeAt(1) - 'A'.charCodeAt(0)) / div : 0
+
+  return [Number(firstInt.toFixed(2)), Number(secondInt.toFixed(2))]
 }
