fd8dea0e896f0dd59005486f9e290502bfd77ce8
0xEpicode <100600187+0xepicode@users.noreply.github.com>
2022-03-25 13:34:45 +0100

    Chore/fix collapse (#283)
    * Feat/send swap updated v2 (#271)
    
    * chore: update send and swap screens
    
    * chore: rename modal, fix width
    
    Co-authored-by: Marek <m.sadura@outlook.com>
    
    * chore: refresh collapse height
    
    * chore: rever chanf
    
    Co-authored-by: Marek <m.sadura@outlook.com>

--- src/components/Atomic/Collapse/useCollapse.ts
+++ src/components/Atomic/Collapse/useCollapse.ts
@@ -1,9 +1,11 @@
-import { useCallback, useRef, useState } from 'react'
+import { useCallback, useRef, useState, useLayoutEffect } from 'react'
 
 const collapseClasses = 'duration-300 ease-in-out transition-max-height'
 
 export const useCollapse = () => {
   const [isActive, setIsActive] = useState(false)
+  const maxHeightRef = useRef(0)
+  const [maxHeight, setMaxHeight] = useState(0)
 
   const contentRef = useRef<HTMLDivElement>(null)
 
@@ -11,7 +13,17 @@ export const useCollapse = () => {
     setIsActive((v) => !v)
   }, [])
 
-  const maxHeight = contentRef.current?.scrollHeight || 0
+  const measure = useCallback(() => {
+    const height = contentRef.current?.scrollHeight || 0
+    if (height !== maxHeightRef.current) {
+      maxHeightRef.current = height
+      setMaxHeight(height)
+    }
+  }, [])
+
+  useLayoutEffect(() => {
+    measure()
+  })
 
   const maxHeightStyle = {
     maxHeight: `${isActive ? maxHeight : 0}px`,
