15597a7935e54aebcb63c07ca3c4ef51c9585182
thorswapdev <thorswap.finance@gmail.com>
2022-04-08 09:05:54 -0400

    chore: change poll interval to 60s

--- src/settings/constants/values.ts
+++ src/settings/constants/values.ts
@@ -10,4 +10,4 @@ export const POLL_GAS_RATE_INTERVAL = 10 * 1000 // 10s
 
 export const POLL_DATA_INTERVAL = 5 * 60 * 1000 // 5m
 
-export const POLL_GET_POOLS_INTERVAL = 30 * 1000 // 30s
+export const POLL_GET_POOLS_INTERVAL = 60 * 1000 // 60s
