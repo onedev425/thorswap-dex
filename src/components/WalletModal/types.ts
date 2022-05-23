c8be908aa19a7931e36b62573427aa7b90b10105
0x Mars <83092433+0xMars4204@users.noreply.github.com>
2022-05-23 10:23:42 +0100

    feat: re-enable doge provider for xdefi wallet (#581)

--- src/components/WalletModal/types.ts
+++ src/components/WalletModal/types.ts
@@ -47,7 +47,7 @@ export const availableChainsByWallet: Record<string, SupportedChain[]> = {
     Chain.Bitcoin,
     Chain.Ethereum,
     Chain.Binance,
-    // Chain.Doge,
+    Chain.Doge,
     Chain.Litecoin,
     Chain.BitcoinCash,
   ],
