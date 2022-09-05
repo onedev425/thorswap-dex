b69b2dda9b1676be2d40a24bc6098c9d40c61c83
chillios <damsznajder@gmail.com>
2022-09-05 17:23:12 +0200

    chore: use npm package

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import { ThemeProvider } from 'components/Theme/ThemeContext';
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 import { useGlobalRefresh } from 'hooks/useGlobalRefresh';
+import Plausible from 'plausible-tracker';
 import { Provider as ReduxProvider } from 'react-redux';
+import { IS_BETA, IS_DEV_API, IS_PROD } from 'settings/config';
 import { store as reduxStore } from 'store/store';
 
 import { checkOrigin } from './helpers/checkOrigin';
@@ -12,6 +14,14 @@ import DrawerProvider from './hooks/useWalletDrawer';
 import Router from './router';
 
 dayjs.extend(duration);
+
+const prefix = IS_PROD ? 'app' : IS_BETA ? 'beta' : IS_DEV_API ? 'dev' : 'localhost';
+const { enableAutoPageviews, enableAutoOutboundTracking } = Plausible({
+  domain: prefix === 'localhost' ? 'development' : `${prefix}.thorswap.finance`,
+});
+
+enableAutoOutboundTracking();
+enableAutoPageviews();
 
 const MainApp = () => {
   useGlobalRefresh();
