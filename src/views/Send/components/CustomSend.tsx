64d81339a63ebcb47bfc5138304a147aadb32ec1
0xEpicode <100600187+0xepicode@users.noreply.github.com>
2022-10-09 15:49:57 +0200

    Chore/hide custom swap tx (#797)
    Co-authored-by: Chillios <chillios@duck.com>
    Co-authored-by: towan <95243956+towanTG@users.noreply.github.com>
    Co-authored-by: mogarchy <45055276+mogarchy@users.noreply.github.com>
    Co-authored-by: msadura <m.sadura@outlook.com>

--- src/views/Send/components/CustomSend.tsx
+++ src/views/Send/components/CustomSend.tsx
@@ -2,7 +2,6 @@ import { Asset, Memo } from '@thorswap-lib/multichain-core';
 import { AssetSelect } from 'components/AssetSelect';
 import { Box, Typography } from 'components/Atomic';
 import { PanelInput } from 'components/PanelInput';
-import { TabsSelect } from 'components/TabsSelect';
 import { ChangeEvent, useCallback, useEffect, useMemo, useState } from 'react';
 import { t } from 'services/i18n';
 import { multichain } from 'services/multichain';
@@ -23,7 +22,7 @@ type Props = {
 
 export const CustomSend = ({ sendAsset, poolAddress, memo, setPoolAddress, setMemo }: Props) => {
   const { inboundAddresses, getInboundData, pools } = useMidgard();
-  const [memoType, setMemoType] = useState(MemoType.deposit);
+  const [memoType] = useState(MemoType.deposit);
   const [outputAsset, setOutputAsset] = useState<Asset>(Asset.RUNE());
 
   const outputAssetsList = useMemo(() => {
@@ -62,14 +61,6 @@ export const CustomSend = ({ sendAsset, poolAddress, memo, setPoolAddress, setMe
     updatePoolAddress();
   }, [updatePoolAddress]);
 
-  const tabs = useMemo(
-    () => [
-      { label: 'Deposit', value: MemoType.deposit },
-      { label: 'Swap', value: MemoType.swap },
-    ],
-    [],
-  );
-
   const handleChangeRecipient = useCallback(
     ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
       setPoolAddress(value);
@@ -86,8 +77,8 @@ export const CustomSend = ({ sendAsset, poolAddress, memo, setPoolAddress, setMe
 
   return (
     <Box col className="self-stretch gap-1" flex={1}>
-      <Box className="self-stretch">
-        <TabsSelect onChange={(val) => setMemoType(val as MemoType)} tabs={tabs} value={memoType} />
+      <Box className="self-stretch px-1.5">
+        <Typography>{t('common.msgDeposit')}</Typography>
       </Box>
 
       {memoType === MemoType.swap && (
