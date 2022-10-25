4cd2f55b1d4f14a4c63b4284e7a4bc3898381b4a
Chillios <chillios@duck.com>
2022-10-25 14:38:33 +0200

    chore: update alchemy provider network id

--- src/services/alchemyProvider.ts
+++ src/services/alchemyProvider.ts
@@ -4,4 +4,4 @@ import { ChainId } from '@thorswap-lib/types';
 let provider: AlchemyProvider;
 
 export const alchemyProvider = () =>
-  (provider ||= new AlchemyProvider(ChainId.Ethereum, import.meta.env.VITE_ALCHEMY_KEY));
+  (provider ||= new AlchemyProvider(parseInt(ChainId.Ethereum), import.meta.env.VITE_ALCHEMY_KEY));
