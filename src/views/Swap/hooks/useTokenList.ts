d4a2002b35d6801a05be02ebc40eaa1b23f186b7
mogarchy <45055276+mogarchy@users.noreply.github.com>
2022-10-28 17:52:33 -0400

    Update useTokenList.ts

--- src/views/Swap/hooks/useTokenList.ts
+++ src/views/Swap/hooks/useTokenList.ts
@@ -6,7 +6,7 @@ import { useAppSelector } from 'store/store';
 import { useGetProvidersQuery } from 'store/thorswap/api';
 import { Token } from 'store/thorswap/types';
 
-export const DISABLED_TOKENLIST_PROVIDERS = IS_PROD ? ['Pangolin', 'Thorchain'] : [];
+export const DISABLED_TOKENLIST_PROVIDERS = IS_PROD ? ['Pangolin'] : [];
 
 export const useTokenList = () => {
   const [tokens, setTokens] = useState<Token[]>([]);
