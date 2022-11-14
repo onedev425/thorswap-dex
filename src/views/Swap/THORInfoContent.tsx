d5369d1bc2f211695161c08abba2c553367d8433
Chillios <chillios@duck.com>
2022-11-14 13:55:36 +0100

    chore: adjust thor info content

--- src/views/Swap/THORInfoContent.tsx
+++ src/views/Swap/THORInfoContent.tsx
@@ -1,19 +1,22 @@
-/* eslint-disable react/no-unescaped-entities */
 import { Asset } from '@thorswap-lib/multichain-core';
-import { Link } from 'components/Atomic';
+import { Link, Typography } from 'components/Atomic';
 import { getSwapRoute } from 'settings/router';
 
-const THORInfoContent = () => {
-  return (
-    <>
-      Are you looking for THORSwap's protocol token $THOR You have selected $THOR token ticker on
-      Avalanche C-Chain. Thor Financial is a different unaffiliated project based on the Avalanche
-      network.
+const THORInfoContent = () => (
+  <>
+    <Typography color="yellow" variant="caption">
+      {`Are you looking for THORSwap's protocol token $THOR?`}
+    </Typography>
+    <Typography variant="caption-xs">&nbsp;</Typography>
+    <Typography color="yellow" variant="caption">
+      {
+        'You have selected $THOR token ticker on Avalanche C-Chain. Thor Financial is a different unaffiliated project based on the Avalanche network. '
+      }
       <Link className="text-twitter-blue" to={getSwapRoute(Asset.THOR())}>
         $THOR (Ethereum ERC-20)
       </Link>
-    </>
-  );
-};
+    </Typography>
+  </>
+);
 
 export default THORInfoContent;
