434ccbb95796bdff32081515d390f2fd8468796e
Chillios <chillios@duck.com>
2022-12-07 17:12:48 +0100

    chore: remove dev for static api

--- src/store/static/api.ts
+++ src/store/static/api.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { IS_DEV_API, IS_STAGENET, STATIC_API } from 'settings/config';
+import { STATIC_API } from 'settings/config';
 import { GetProviderTokensParams } from 'store/thorswap/types';
 
 export const staticApi = createApi({
@@ -11,7 +11,7 @@ export const staticApi = createApi({
   }),
   endpoints: (build) => ({
     getTokenList: build.query<GetProviderTokensParams, string>({
-      query: (name) => `/token-list/${IS_DEV_API || IS_STAGENET ? 'dev/' : ''}${name}.json`,
+      query: (name) => `/token-list/${name}.json`,
     }),
   }),
 });
