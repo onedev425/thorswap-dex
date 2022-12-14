41eb605ac4eaf891eabd701e597a9d28b662b4c7
mfaizan1 <mfaizanulhaq1@gmail.com>
2022-12-14 13:38:12 +0500

    refactor: remove plausible

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,8 @@ import { ThemeProvider } from 'components/Theme/ThemeContext';
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 import { useGlobalRefresh } from 'hooks/useGlobalRefresh';
-import Plausible from 'plausible-tracker';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider as ReduxProvider } from 'react-redux';
-import { IS_BETA, IS_DEV_API, IS_PROD } from 'settings/config';
 import { store as reduxStore } from 'store/store';
 
 import { checkOrigin } from './helpers/checkOrigin';
@@ -16,14 +14,6 @@ import DrawerProvider from './hooks/useWalletDrawer';
 import { PublicRoutes } from './router';
 
 dayjs.extend(duration);
-
-const prefix = IS_PROD ? 'app' : IS_BETA ? 'beta' : IS_DEV_API ? 'dev' : 'localhost';
-const { enableAutoPageviews, enableAutoOutboundTracking } = Plausible({
-  domain: prefix === 'localhost' ? 'development' : `${prefix}.thorswap.finance`,
-});
-
-enableAutoOutboundTracking();
-enableAutoPageviews();
 
 const MainApp = () => {
   useGlobalRefresh();
