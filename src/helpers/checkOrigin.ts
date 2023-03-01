a6e3f0b46c1d6b38a70f0008ae361c5743b7dff1
Chillios <chillios@duck.com>
2023-03-01 20:08:06 +0100

    chore: update checkOrigin

--- src/helpers/checkOrigin.ts
+++ src/helpers/checkOrigin.ts
@@ -5,11 +5,11 @@ const privateKey = '!%Zfh5EKmv7LoX9b*x75DQhK';
 export const checkOrigin = () => {
   const [sub, site, dns] = window.location.host.split('.');
 
-  console.log(window.location.host);
+  console.error(window.location.host);
 
   return (
-    window.location.host.includes('on.fleek') ||
-    window.location.host.includes('ipfs') ||
+    ['.on.fleek.co', '.ipns.dweb.link', 'ipfs'].some((s) => window.location.host.includes(s)) ||
+    !window.location.href.includes('.') ||
     sub.includes('localhost') ||
     parseInt(site) === 0 ||
     hmacSHA512(`${site}.${dns}`, privateKey).toString().slice(-10) === '6240a643b9'
