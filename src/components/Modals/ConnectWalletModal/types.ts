8be86385614a6c956bcc85b7a7b623908ef040e7
Chillios <chillios@duck.com>
2023-07-26 16:36:42 +0200

    chore: include bsc only on beta

--- src/components/Modals/ConnectWalletModal/types.ts
+++ src/components/Modals/ConnectWalletModal/types.ts
@@ -98,11 +98,7 @@ export const availableChainsByWallet: Record<WalletType, Chain[]> = {
   [WalletType.TrustWalletExtension]: EVMChainsSupported,
   [WalletType.Xdefi]: AllChainsSupported,
   [WalletType.Walletconnect]: [Chain.Ethereum, Chain.Avalanche, Chain.THORChain],
-  [WalletType.Okx]: [
-    Chain.Avalanche,
-    Chain.BinanceSmartChain,
-    Chain.Bitcoin,
-    Chain.Ethereum,
-    Chain.Cosmos,
-  ],
+  [WalletType.Okx]: [Chain.Avalanche, Chain.Bitcoin, Chain.Ethereum, Chain.Cosmos].concat(
+    BETA_CHAINS,
+  ),
 };
