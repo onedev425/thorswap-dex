ab66fd801df21d41571992cf0c05a4bb9c07b57f
towan <95243956+towanTG@users.noreply.github.com>
2023-08-10 01:14:55 +0400

    fix: a couple of bugs (#1217)

--- src/views/Wallet/components/HeaderChainInfo.tsx
+++ src/views/Wallet/components/HeaderChainInfo.tsx
@@ -1,9 +1,13 @@
 import { Text } from '@chakra-ui/react';
 import { ChainWallet } from '@thorswap-lib/swapkit-core';
-import { Chain } from '@thorswap-lib/types';
+import { Chain, WalletOption } from '@thorswap-lib/types';
 import { Box } from 'components/Atomic';
+import { PhraseModal } from 'components/Modals/PhraseModal';
+import { showInfoToast } from 'components/Toast';
 import { WalletIcon } from 'components/WalletIcon/WalletIcon';
 import { chainName } from 'helpers/chainName';
+import { t } from 'i18next';
+import { useCallback, useState } from 'react';
 
 type Props = {
   chain: Chain;
@@ -12,9 +16,33 @@ type Props = {
 };
 
 export const HeaderChainInfo = ({ chain, chainWallet, balance }: Props) => {
+  const [isPhraseModalVisible, setIsPhraseModalVisible] = useState(false);
+
+  const handleClosePhraseModal = () => {
+    setIsPhraseModalVisible(false);
+  };
+  const handleClickWalletIcon = useCallback(async () => {
+    if (chainWallet?.walletType === WalletOption.KEYSTORE) {
+      setIsPhraseModalVisible(true);
+    }
+    const { getAddress } = await (await import('services/swapKit')).getSwapKitClient();
+
+    if (chainWallet?.walletType === WalletOption.LEDGER) {
+      showInfoToast(t('notification.verifyLedgerAddy'), getAddress(chain), {
+        duration: 20 * 1000,
+      });
+    }
+  }, [chainWallet?.walletType, chain]);
+
   return (
     <Box center className="space-x-1">
-      {!!chainWallet && <WalletIcon size={16} walletType={chainWallet?.walletType} />}
+      {!!chainWallet && (
+        <WalletIcon
+          onClick={handleClickWalletIcon}
+          size={16}
+          walletType={chainWallet?.walletType}
+        />
+      )}
       <Text>{chainName(chain, true)}</Text>
 
       {!!chainWallet && (
@@ -22,6 +50,7 @@ export const HeaderChainInfo = ({ chain, chainWallet, balance }: Props) => {
           {balance}
         </Text>
       )}
+      <PhraseModal isOpen={isPhraseModalVisible} onCancel={handleClosePhraseModal} />
     </Box>
   );
 };
