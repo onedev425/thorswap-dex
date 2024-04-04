fb29fb6f225a0472593117800f25994fa44318e8
towan <95243956+towanTG@users.noreply.github.com>
2024-04-04 20:52:35 +0200

    chore: adds USDT as cf supported asset (#1570)
    Co-authored-by: towanTG <namelessthorguard@gmail.com>

--- src/views/Swap/hooks/useSwapQuote.ts
+++ src/views/Swap/hooks/useSwapQuote.ts
@@ -30,6 +30,7 @@ const chainflipSupportedAssets = [
   'BTC.BTC',
   'DOT.DOT',
   'ETH.USDC-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  'ETH.USDT-0xdac17f958d2ee523a2206206994597c13d831ec7',
   'ETH.FLIP-0x826180541412d574cf1336d22c0c0a287822678a',
 ];
 
