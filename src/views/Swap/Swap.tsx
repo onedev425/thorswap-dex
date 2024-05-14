67d941a8eeb2a24d4a3849833a7527558d895656
towan <towan@thorswap.finance>
2024-05-14 14:37:51 +0400

    fix: tiny ui bug

--- src/views/Swap/Swap.tsx
+++ src/views/Swap/Swap.tsx
@@ -242,7 +242,7 @@ const SwapView = () => {
 
   const amountTooLowForLimit = useMemo(
     () =>
-      (selectedRouteRaw || inputUSDPrice) &&
+      selectedRouteRaw &&
       new SwapKitNumber({
         value:
           inputUSDPrice ||
