ce759d49706b46ebc26a7119f8490e88c21720a5
0xEpicode <100600187+0xepicode@users.noreply.github.com>
2024-05-22 17:53:45 +0200

    Ability to set color theme from query param (#1661)

--- src/context/theme/useThemeState.ts
+++ src/context/theme/useThemeState.ts
@@ -4,10 +4,26 @@ import { useApp } from 'store/app/hooks';
 import { ThemeType } from 'types/app';
 
 export const useThemeState = () => {
-  const { themeType } = useApp();
+  const getThemeFromQuery = () => {
+    const params = new URLSearchParams(window.location.search);
+    const theme = params.get('theme');
+
+    switch (theme) {
+      case ThemeType.Light:
+        return ThemeType.Light;
+      case ThemeType.Dark:
+        return ThemeType.Dark;
+      default:
+        return null;
+    }
+  };
+
+  const [themeFromQuery, setThemeFromQuery] = useState(getThemeFromQuery);
+  const defaultTheme = getThemeFromQuery();
+  const { themeType, setTheme } = useApp();
   const { setColorMode } = useColorMode();
 
-  const [activeTheme, setActiveTheme] = useState(ThemeType.Dark);
+  const [activeTheme, setActiveTheme] = useState(defaultTheme || themeType);
 
   const activateTheme = useCallback(
     (updatedTheme: ThemeType) => {
@@ -26,8 +42,15 @@ export const useThemeState = () => {
   );
 
   useEffect(() => {
+    if (themeFromQuery) {
+      activateTheme(themeFromQuery);
+      setTheme(themeFromQuery);
+      setThemeFromQuery(null);
+      return;
+    }
+
     activateTheme(themeType);
-  }, [activateTheme, themeType]);
+  }, [activateTheme, setTheme, themeFromQuery, themeType]);
 
   return { theme: activeTheme, isLight: activeTheme === ThemeType.Light };
 };
